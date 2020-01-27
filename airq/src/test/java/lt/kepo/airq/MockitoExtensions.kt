d431f9853e6f637b3be007b55a58f92e9fc82101
kestutispovilaitis <kiesiusp@gmail.com>
2020-01-28 00:44:11 +0200

    removed test dependencies

--- airq/src/test/java/lt/kepo/airq/MockitoExtensions.kt
+++
@@ -1,5 +0,0 @@
-package lt.kepo.airq
-
-import org.mockito.Mockito
-
-inline fun <reified T> mock(): T = Mockito.mock(T::class.java)
--- airq/src/test/java/lt/kepo/airq/utility/Functions.kt
+++
@@ -1,7 +0,0 @@
-package lt.kepo.airq.utility
-
-import android.content.Context
-
-fun isLocationEnabled(context: Context): Boolean {
-    return false
-}

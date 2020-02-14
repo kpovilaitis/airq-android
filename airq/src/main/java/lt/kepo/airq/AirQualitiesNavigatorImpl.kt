7baebb46f36286405d197e7cc6c53436e7dddb17
kpovilaitis <kpovilaitis@shift4.com>
2020-02-14 12:21:59 +0200

    Navigator : fixed animation overriding

--- airq/src/main/java/lt/kepo/airq/AirQualitiesNavigatorImpl.kt
+++ airq/src/main/java/lt/kepo/airq/AirQualitiesNavigatorImpl.kt
@@ -2,6 +2,7 @@ package lt.kepo.airq
 
 import android.app.Activity
 import android.content.Intent
+import android.os.Build
 import lt.kepo.airquality.R
 import lt.kepo.airquality.ui.AirQualityActivity
 import lt.kepo.core.ui.AppNavigator
@@ -12,14 +13,20 @@ class AirQualitiesNavigatorImpl : AppNavigator {
     override fun startAirQualityActivity(activity: Activity) {
         activity.apply {
             startActivity(Intent(activity, AirQualityActivity::class.java))
-            overridePendingTransition(R.anim.window_enter, R.anim.window_exit)
+
+            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.O_MR1) { // overriding default anim in new android looks bad
+                overridePendingTransition(R.anim.window_enter, R.anim.window_exit)
+            }
         }
     }
 
     override fun startStationsActivity(activity: Activity) {
         activity.apply {
             startActivity(Intent(activity, StationsActivity::class.java))
-            overridePendingTransition(R.anim.window_enter, R.anim.window_exit)
+
+            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.O_MR1) { // overriding default anim in new android looks bad
+                overridePendingTransition(R.anim.window_enter, R.anim.window_exit)
+            }
         }
     }
 }
